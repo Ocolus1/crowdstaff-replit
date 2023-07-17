f98e90784aea21c34f59c4000612f440cfe14c81
Paul Awolesi <47501949+Ocolus1@users.noreply.github.com>
2023-07-17 14:37:50 +0100

    made the output more readable

--- main.py
+++ main.py
@@ -100,32 +100,33 @@ class Scraper:
                 organization = None
             job_position = self.driver.find_element(By.XPATH,'.//p[@class="sc-bLqSdX VuJyj"]').text
             job_code = self.driver.find_element(By.XPATH,'.//p[@class="sc-kgDbTy hjUIEE"]').text
-            location = self.driver.find_element(By.XPATH,'.//p[@class="sc-ilsNdd hDiSpo"]/span').text
+            location = self.driver.find_element(By.XPATH,'.//p[@class="sc-ilsNdd hDiSpo"]/span[2]').text.split("in ")[1]
             divs = self.driver.find_elements(By.XPATH,'.//div[@class="sc-jMWxgM kXMJoW wysiwyg-output"]')
-            summary = divs[0].text.split('\n') if len(divs) > 0 else []
-            roles_responsibilities = divs[1].text.split('\n') if len(divs) > 1 else []
+            summary = "\n".join(divs[0].text.split('\n')) if len(divs) > 0 else ""
+            roles_responsibilities = "\n".join(divs[1].text.split('\n')) if len(divs) > 1 else ""
             divs = self.driver.find_elements(By.XPATH,'.//div[@class="sc-dNkQa kWYEjC text-12p"]')
-            required_skills = [div.text for div in divs]
+            required_skills = " ".join([div.text for div in divs])
             if len(required_skills) == 0: 
                 required_skills = None
             divs = self.driver.find_elements(By.XPATH, './/ul[@class="sc-dIMhSk jIrqzB"]/div')
             lis = {}
-            keys = ['Available', 'Experience', 'Term', 'Starting', 'Type:', 'Auth:', 'Certifications:', 'Benefits:', 'Rate:\nUSD\n$', 'Rate:\nUSD\n$']
+            keys = ['Available', 'Experience', 'Term', 'Starting', 'Type:', 'Auth:', 'Certifications:', 'Benefits:', 'Pay Rate:', 'Reward Rate:']
             for i, div in enumerate(divs):
                 text = div.find_element(By.TAG_NAME, 'li').text
                 for key in keys:
                     if key in text:
                         lis[key] = text
-            position = lis.get('Available', None)
-            years_of_experience  = lis.get('Experience', None)
-            contract_length = lis.get('Term', None)
-            starting_period  = lis.get('Starting', None)
-            work_type = lis.get('Type:', None)
-            work_auth = lis.get('Auth:', None)
-            certifications = lis.get('Certifications:', None)
-            benefits = lis.get('Benefits:', None)
-            pay_rate = lis.get('Rate:\nUSD\n$', None)
-            reward_rate = lis.get('Rate:\nUSD\n$', None)
+            position = lis.get('Available').split(' ')[0] if lis.get('Available') is not None else None
+            years_of_experience  = lis.get('Experience').split(' Experience')[0] if lis.get('Experience') is not None else None
+            contract_length = lis.get('Term').split(' Term')[0] if lis.get('Term') is not None else None
+            starting_period  = lis.get('Starting').split('Starting ')[1] if lis.get('Starting') is not None else None
+            work_type = lis.get('Type:').split(': ')[1] if lis.get('Type:') is not None else None
+            work_auth = lis.get('Auth:').split(': ')[1] if lis.get('Auth:') is not None else None
+            certifications = lis.get('Certifications:').split(': ')[1] if lis.get('Certifications:') is not None else None
+            benefits = lis.get('Benefits:').split(': ')[1] if lis.get('Benefits:') is not None else None
+            pay_rate = "$" + lis.get('Pay Rate:').split('$')[1].replace("\n", "") if lis.get('Pay Rate:') is not None else None
+            reward_rate = "$" + lis.get('Reward Rate:').split('$')[1].replace("\n", "") if lis.get('Reward Rate:') is not None else None
+
             
             job_info.append({
                 'organization': organization,
@@ -138,6 +139,8 @@ class Scraper:
                 'position': position,
                 'years_of_experience': years_of_experience,
                 'contract_length': contract_length,
+                'starting_period': starting_period,
+                'work_type': work_type,
                 'work_auth': work_auth,
                 'certifications': certifications,
                 'benefits': benefits,
@@ -165,4 +168,5 @@ class Scraper:
 
 if __name__ == '__main__':
     scraper = Scraper()
-    scraper.main('emailaddress', 'password!')
+    scraper.main('randy@napkinhealth.com', 'Cleanuponaisle6!')
+    # scraper.main('emailaddress', 'password!')
